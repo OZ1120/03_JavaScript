eae0c3ff8815262de1c079af3e74b1464e6b54e3
OZ <macoto8@naver.com>
2024-01-12 17:44:32 +0900

    24012_자바스크립트
    06_입출금하기(숙제)

--- js/06_입출금하기.js
+++ js/06_입출금하기.js
@@ -22,6 +22,45 @@ function test(){
 
 const amount = document.getElementById("input");
 
-let balance
+let balance = document.getElementById("balance");
+// let balance = 10000;
 
-const password =
+const password = 1234;
+
+
+function deposit(){ //입금
+
+}
+
+
+function withdrawal(){ //출금
+
+
+
+    const password = prompt("비밀번호를 입력하세요");
+
+    if(password == null){
+        alert("취소")
+    } else{
+        if(password == 1234){
+            alert("비밀번호 일치")
+        } else {
+            alert("비밀번호가 일치하지 않슴")
+        }
+    }
+
+}
+
+
+
+ 밸런스에 입금(더하기)값 출력
+ 밸런스에 출금(빼기) 값 출력
+ 인데 이제 10000에서부터 시작
+ 인데 0이하는 안되고
+ 패스워드는 1234 이외에 안됨
+ 흠?
+ 갸아아아아앙아아아아앙아앙아아몰라몰랄몰라몰라몰라몰라몰라
+
+balance= balance + input
+
+balance.innerText = balance
